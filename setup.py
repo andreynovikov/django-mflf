6c9ae096dc4a55b25c3f20e06b485588eff1fa0e
Andrey Novikov <novikov@gmail.com>
2022-04-01 17:16:14 +0300

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='django-mflf',
-    version='0.0.1',
+    version='0.1.0',
     license='MIT',
     author='Andrey Novikov',
     author_email='novikov@gmail.com',
