86776ccffb03ce4caea422e4334dbdbb6f68d50a
Andrey Novikov <novikov@gmail.com>
2022-04-01 17:10:43 +0300

    Fix test for Django 4

--- tests/testapp/urls_admin.py
+++ tests/testapp/urls_admin.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+try:
+    from django.urls import re_path
+except ImportError:  # Django 1.11
+    from django.conf.urls import url as re_path
 from django.contrib import admin
-from reviews.admin import ReviewAdmin
-from reviews.models import Review
 
-from testapp.models import Article
+from testapp.models import Product
 
 
 admin_site = admin.AdminSite()
-admin_site.register(Review, ReviewAdmin)
-admin_site.register(Article)
+admin_site.register(Product)
 
 urlpatterns = [
-    url(r'^admin/', admin_site.urls),
+    re_path(r'^admin/', admin_site.urls),
 ]
